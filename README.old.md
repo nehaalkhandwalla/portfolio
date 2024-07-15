5aa9f074b130df412fbc7cc8ac7cd18654d0f1a0
Nehaal Khandwalla <nehaalkhandwalla1@gmail.com>
2024-07-15 15:29:27 +0100

    Home page dynamic text

--- README.old.md
+++ README.old.md
@@ -4,3 +4,5 @@ Personal Portfolio
 Installations:
 
 React Router Dom: npm install react-router-dom
+React Scroll: npm install react-scroll
+React typed: npm install typed.js
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import Typed from 'typed.js';
 
 const Home = () => {
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      strings: ["STUDENT", "WEB DEVELOPER", "WEB DESIGNER", "HAAFIDHAH", "ICE-SKATER"],
+      typeSpeed: 50,
+      backSpeed: 20,
+      backDelay: 2000,
+      showCursor: true,
+      loop: true,
+    };
+
+    const typed = new Typed(textRef.current, options);
+
+    return () => {
+      // Make sure to destroy Typed instance during cleanup to prevent memory leaks
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div id="home" className="section">
-      <h1>Home</h1>
-      <p>Welcome to my portfolio!</p>
+      <section className="hero">
+        <div className="hero-text">
+          <p> 
+            I AM A <span ref={textRef}></span> 
+          </p>
+        </div>
+      </section>
     </div>
   );
 };
