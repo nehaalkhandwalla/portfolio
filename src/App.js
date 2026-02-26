7634d6db7a9f383dbe9069b316d79f7782a68f60
Nehaal Khandwalla <nehaalkhandwalla1@gmail.com>
2026-02-26 20:13:50 +0000

    Revert "Updates"
    This reverts commit cc9307819fea3540da9619516f6659c01a50e180.

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css'; // Ensure this path matches your directory structure
+import Navbar from './Navbar'; // Ensure this path matches your directory structure
+import Footer from './Footer';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import About from './pages/About';
+import Experience from './pages/Experience';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <div id="home" className="section">
+        <Home />
+      </div>
+      <div id="about" className="section">
+        <About />
+      </div>
+      <div id="projects" className="section">
+        <Projects />
+      </div>
+      <div id="experience" className="section">
+        <Experience />
+      </div>
+      <div id="footer" className="footer-section">
+      <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef, useState } from 'react';
+import Typed from 'typed.js';
+import profileimg from '../nehaal.png';
+
+
+const Home = () => {
+  const textRef = useRef(null);
+  const [prefix, setPrefix] = useState("I AM A");
+
+  useEffect(() => {
+    const options = {
+      strings: ["MUSLIM", "DEVELOPER", "STUDENT", "TUTOR", "ICE-SKATER"],
+      typeSpeed: 50,
+      backSpeed: 20,
+      backDelay: 2000,
+      showCursor: true,
+      loop: true,
+      preStringTyped: (arrayPos) => {
+        const currentString = options.strings[arrayPos];
+        const firstLetter = currentString.charAt(0).toLowerCase();
+        const newPrefix = 'aeiou'.includes(firstLetter) ? 'I AM AN' : 'I AM A';
+        setPrefix(newPrefix);
+      },
+    };
+
+    const typed = new Typed(textRef.current, options);
+
+    return () => {
+      // Cleanup to prevent memory leaks
+      typed.destroy();
+    };
+  }, []);
+
+  return (
+    <div id="home" className="section">
+        <div className='profile-pic'>
+            <img src={profileimg} alt="Profile" />
+        </div>
+        
+      <section className="hero">
+        <div className="hero-text">
+          <p className='hero-sentence'>
+            {prefix} <span ref={textRef}></span>
+          </p>
+        </div>
+      </section>
+      
+    </div>
+  );
+};
+
+export default Home;
